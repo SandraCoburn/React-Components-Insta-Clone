7bac908b9d925f34b693c392cacf8682a62fc63e
Sandra Coburn <sandra_coburn@hotmail.com>
2020-04-13 14:39:16 -0700

    mvp

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -4,12 +4,17 @@ import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
-const CommentSection = props => {
+const CommentSection = (props) => {
+  console.log("propes comments", props);
   // Add state for the comments
-
+  const [comments, seetComments] = useState();
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {props.comments.map((comment) => {
+        return <Comment key={comment.id} comment={comment} />;
+      })}
+
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,23 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
+import dummyData from "../../dummy-data";
 
 const PostsPage = () => {
   // set up state for your data
+  const [post, setPost] = useState([]);
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {dummyData.map((post) => {
+        return <Post key={post.id} post={post} setPost={setPost} />;
+      })}
     </div>
   );
 };
 
 export default PostsPage;
-
